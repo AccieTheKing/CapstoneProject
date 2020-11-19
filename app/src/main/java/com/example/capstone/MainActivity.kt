4b040deba4d1bcac1ef95ceb272c060fb1ce8030
Acdaling <acdaling@gmail.com>
2020-11-19 22:56:38 +0100

    Created methods for changing the fragment view and setting active icon

--- app/src/main/java/com/example/capstone/MainActivity.kt
+++ app/src/main/java/com/example/capstone/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.capstone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment() // holds the home fragment
+        val profileFragment = ProfileFragment() // holds the profile fragment
+        val menuFragment = MenuFragment() // holds the menu fragment
+
+        setCurrentFragment(homeFragment) // on create set the home as active
+        bottomNavigationView.selectedItemId = R.id.homeIconNav // sets the home as active
+
+        // listens to clicks bottom nav
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.homeIconNav -> setCurrentFragment(homeFragment)
+                R.id.personIconNav -> setCurrentFragment(profileFragment)
+                R.id.menuIconNav -> setCurrentFragment(menuFragment)
+            }
+            true
+        }
+    }
+
+
+    /**
+     * This method will set the given fragment as the shown fragment
+     * inside the frame layout on the activity home screen
+     */
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
     }
 }
