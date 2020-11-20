1e4b5dcc3c6702b398f7c329080719e25562df91
Acdaling <acdaling@gmail.com>
2020-11-20 21:58:22 +0100

    Initialized the adapters on the home and menu fragment

--- app/src/main/java/com/example/capstone/ui/HomeFragment.kt
+++ app/src/main/java/com/example/capstone/ui/HomeFragment.kt
@@ -1,17 +1,21 @@
 package com.example.capstone.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.capstone.R
+import com.example.capstone.models.Announcement
+import com.example.capstone.ui.adapters.AnnouncementAdapter
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val announcements = arrayListOf<Announcement>()
+    private lateinit var announcementAdapter: AnnouncementAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +23,21 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        announcementAdapter = AnnouncementAdapter(announcements)
+        rcHomeFragment.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        rcHomeFragment.adapter = announcementAdapter
+        DividerItemDecoration(
+            context,
+            DividerItemDecoration.VERTICAL
+        )
+        announcementAdapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/capstone/ui/MenuFragment.kt
+++ app/src/main/java/com/example/capstone/ui/MenuFragment.kt
@@ -5,9 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.capstone.R
+import com.example.capstone.models.Product
+import com.example.capstone.ui.adapters.ProductAdapter
+import kotlinx.android.synthetic.main.fragment_menu.*
 
 class MenuFragment : Fragment() {
+    private val products = arrayListOf<Product>()
+    private lateinit var productAdapter: ProductAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +23,23 @@ class MenuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_menu, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        productAdapter = ProductAdapter(products)
+        rcMenuFragment.layoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
+        rcMenuFragment.adapter = productAdapter
+        rcMenuFragment.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+        productAdapter.notifyDataSetChanged()
     }
 }
