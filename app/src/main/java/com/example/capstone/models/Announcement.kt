42cdbc80b333cdec9820ad4d0a6d31ef50446d4b
Acdaling <acdaling@gmail.com>
2020-11-20 15:46:19 +0100

    Added comment to the Announcement model

--- app/src/main/java/com/example/capstone/models/Announcement.kt
+++ app/src/main/java/com/example/capstone/models/Announcement.kt
@@ -2,6 +2,12 @@ package com.example.capstone.models
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * @model Announcement
+ *
+ * This model is meant to be used on the Home fragment.
+ * Here the news of the store or restaurant can be found
+ */
 data class Announcement(
     @SerializedName("title") var title: String,
     @SerializedName("banner_image") var banner_image: String,
