d1d31cd2206484cb1b19f57ffe2e0c988729bf74
Acdaling <acdaling@gmail.com>
2020-11-20 15:45:14 +0100

    Created the Product model
    - This model is going to be used on the menu fragment and the user can add them to a list and eventually buy them

---
+++ app/src/main/java/com/example/capstone/models/Product.kt
@@ -0,0 +1,16 @@
+package com.example.capstone.models
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * @model Product
+ *
+ * This model is going to be used on the menu fragment and the user can add them
+ * to a list and eventually buy them
+ */
+data class Product(
+    @SerializedName("title") var title: String,
+    @SerializedName("banner_image") var banner_image: String,
+    @SerializedName("description") var description: String,
+    @SerializedName("price") var price: Double,
+)
