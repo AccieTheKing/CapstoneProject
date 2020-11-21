601e0d32f18eb1ed01667edee4b525647e0dd310
Acdaling <acdaling@gmail.com>
2020-11-21 19:19:19 +0100

    Attached the checkout adapter to the checkout fragment

--- app/src/main/java/com/example/capstone/ui/CartFragment.kt
+++ app/src/main/java/com/example/capstone/ui/CartFragment.kt
@@ -6,11 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.capstone.R
+import com.example.capstone.models.Product
+import com.example.capstone.ui.adapters.CheckoutAdapter
 import kotlinx.android.synthetic.main.fragment_cart.*
 
 class CartFragment : Fragment() {
     private var showCheckout: Boolean = false
+    private val products = arrayListOf<Product>()
+    private lateinit var checkoutAdapter: CheckoutAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +33,20 @@ class CartFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
     }
 
     private fun initView() {
         btnBuyProducts.isVisible = showCheckout
+        checkoutAdapter = CheckoutAdapter(products)
+        rcCheckoutList.layoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
+        rcCheckoutList.adapter = checkoutAdapter
+        rcCheckoutList.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+        checkoutAdapter.notifyDataSetChanged()
     }
 }
