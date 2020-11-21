9751c91a21b0215ca858d1112e1bb0bda1c640ea
Acdaling <acdaling@gmail.com>
2020-11-21 18:52:01 +0100

    made mock implementation of showing the checkout button

--- app/src/main/java/com/example/capstone/ui/CartFragment.kt
+++ app/src/main/java/com/example/capstone/ui/CartFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.example.capstone.R
+import kotlinx.android.synthetic.main.fragment_cart.*
 
 class CartFragment : Fragment() {
+    private var showCheckout: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +22,13 @@ class CartFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cart, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun initView() {
+        btnBuyProducts.isVisible = showCheckout
     }
 }
