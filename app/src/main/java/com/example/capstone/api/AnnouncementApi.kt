e0f0036b3db93680af780c7ef7f473471c4e3250
Acdaling <acdaling@gmail.com>
2020-11-22 20:11:15 +0100

    added a companion object

--- app/src/main/java/com/example/capstone/api/AnnouncementApi.kt
+++ app/src/main/java/com/example/capstone/api/AnnouncementApi.kt
@@ -7,26 +7,28 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class AnnouncementApi {
-    // The base url off the api.
-    private const val baseUrl = ""
+    companion object {
+        // The base url off the api.
+        private const val baseUrl = ""
 
-    /**
-     * @return [AnnouncementApiService] The service class off the retrofit client.
-     */
-    fun createApi(): AnnouncementApiService {
-        // Create an OkHttpClient to be able to make a log of the network traffic
-        val okHttpClient = OkHttpClient.Builder()
-            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-            .build()
+        /**
+         * @return [AnnouncementApiService] The service class off the retrofit client.
+         */
+        fun createApi(): AnnouncementApiService {
+            // Create an OkHttpClient to be able to make a log of the network traffic
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .build()
 
-        // Create the Retrofit instance
-        val announcementApi = Retrofit.Builder()
-            .baseUrl(baseUrl)
-            .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+            // Create the Retrofit instance
+            val announcementApi = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
 
-        // Return the Retrofit AnnouncementApiService
-        return announcementApi.create(AnnouncementApiService::class.java)
+            // Return the Retrofit AnnouncementApiService
+            return announcementApi.create(AnnouncementApiService::class.java)
+        }
     }
 }
