348ab34c9affafe76d90b7ec59e2b3c2f5fae147
Acdaling <acdaling@gmail.com>
2020-11-22 21:25:49 +0100

    Changed the products endpoint of adding product to the cart

--- app/src/main/java/com/example/capstone/services/ProductApiService.kt
+++ app/src/main/java/com/example/capstone/services/ProductApiService.kt
@@ -10,15 +10,15 @@ interface ProductApiService {
     suspend fun getProducts(): List<Product>
 
     @GET("/products/{id}")
-    suspend fun getProduct(@Path("id") id: String): Product
+    suspend fun getProduct(@Path("id") id: Int): Product
 
-    @POST("/order/add/{product_id}/{profile_id}")
+    @POST("/cart/add/{product_id}/{profile_id}")
     suspend fun addProductToCart(
         @Path("product_id") product_id: Int,
         @Path("profile_id") profile_id: Int
     ): List<Product>
 
-    @POST("/order/remove/{product_id}/{profile_id}")
+    @POST("/cart/remove/{product_id}/{profile_id}")
     suspend fun removeProductFromCart(
         @Path("product_id") product_id: Int,
         @Path("profile_id") profile_id: Int
