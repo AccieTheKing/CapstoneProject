89542532acd8d47c25f460695159bb4b0bbda448
Acdaling <acdaling@gmail.com>
2020-11-22 21:25:22 +0100

    Finished the products repository

---
+++ app/src/main/java/com/example/capstone/repository/ProductRepository.kt
@@ -0,0 +1,49 @@
+package com.example.capstone.repository
+
+import androidx.lifecycle.MutableLiveData
+import com.example.capstone.api.ProductApi
+import com.example.capstone.models.Product
+import com.example.capstone.services.ProductApiService
+
+class ProductRepository {
+    private val productApiService: ProductApiService = ProductApi.createApi()
+
+    private val _product: MutableLiveData<Product> = MutableLiveData() // single products
+    private val _products: MutableLiveData<List<Product>> =
+        MutableLiveData() // list of all products
+    private val _cart: MutableLiveData<List<Product>> = MutableLiveData() // user cart
+
+    /**
+     * This method will give a list of products available
+     */
+    suspend fun getProducts() {
+        val result = productApiService.getProducts()
+        _products.value = result
+    }
+
+    /**
+     * This method will give a specific product based on the id given
+     */
+    suspend fun getProduct(product_id: Int) {
+        val result = productApiService.getProduct(product_id)
+        _product.value = result
+    }
+
+    /**
+     * This method will add a product to the user cart and return the list
+     * without this product
+     */
+    suspend fun addProductToCart(product_id: Int) {
+        val result = productApiService.addProductToCart(product_id)
+        _cart.value = result
+    }
+
+    /**
+     * This method will remove a product out of the user cart and return the list
+     * without this product
+     */
+    suspend fun removeProductToCart(product_id: Int) {
+        val result = productApiService.removeProductFromCart(product_id)
+        _cart.value = result
+    }
+}
