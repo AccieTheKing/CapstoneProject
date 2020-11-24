e1ca8420ccbf527bc506289ed0a3b361b8fb3c12
Acdaling <acdaling@gmail.com>
2020-11-24 11:23:28 +0100

    initialized the producsViewModel into the product fragment

--- app/src/main/java/com/example/capstone/ui/ProductsFragment.kt
+++ app/src/main/java/com/example/capstone/ui/ProductsFragment.kt
@@ -5,16 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.capstone.R
 import com.example.capstone.models.Product
+import com.example.capstone.models.Profile
 import com.example.capstone.ui.adapters.ProductAdapter
+import com.example.capstone.ui.viewmodels.ProductViewModel
 import kotlinx.android.synthetic.main.fragment_products.*
 
 class ProductsFragment : Fragment() {
     private val products = arrayListOf<Product>()
+    private val viewModel: ProductViewModel by viewModels()
     private lateinit var productAdapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,10 +37,11 @@ class ProductsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initView()
+        observeProducts()
     }
 
     private fun initView() {
-        productAdapter = ProductAdapter(products)
+        productAdapter = ProductAdapter(products, ::addToCart)
         rcProductsFragment.layoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
         rcProductsFragment.adapter = productAdapter
         rcProductsFragment.addItemDecoration(
@@ -46,6 +51,19 @@ class ProductsFragment : Fragment() {
             )
         )
         productAdapter.notifyDataSetChanged()
+    }
+
+    private fun observeProducts() {
+        viewModel.getProducts()
+        viewModel.products.observe(viewLifecycleOwner, {
+            products.clear()
+            products.addAll(it)
+            productAdapter.notifyDataSetChanged()
+        })
+    }
+
+    private fun addToCart(product: Product){
+        viewModel.addProductToCart(product, Profile("0640117445", "acdaling@gmail.com", "https://images.acdaling.nl/me.jpg"))
     }
 
 }
