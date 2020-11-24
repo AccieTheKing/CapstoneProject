7bdfbcd5b90e9ebdfc09f839ede83af12e8890d4
Acdaling <acdaling@gmail.com>
2020-11-24 10:52:15 +0100

    Gave models an ID

--- app/src/main/java/com/example/capstone/models/Announcement.kt
+++ app/src/main/java/com/example/capstone/models/Announcement.kt
@@ -9,6 +9,7 @@ import com.google.gson.annotations.SerializedName
  * Here the news of the store or restaurant can be found
  */
 data class Announcement(
+    @SerializedName("id") var id: Int,
     @SerializedName("title") var title: String,
     @SerializedName("banner_image") var banner_image: String,
     @SerializedName("text") var text: String,
--- app/src/main/java/com/example/capstone/models/Product.kt
+++ app/src/main/java/com/example/capstone/models/Product.kt
@@ -9,6 +9,7 @@ import com.google.gson.annotations.SerializedName
  * to a list and eventually buy them
  */
 data class Product(
+    @SerializedName("id") var id: Int,
     @SerializedName("title") var title: String,
     @SerializedName("banner_image") var banner_image: String,
     @SerializedName("description") var description: String,
