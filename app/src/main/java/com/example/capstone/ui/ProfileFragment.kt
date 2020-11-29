2291357c4e51308c0c665577dc4601efbfa14adf
Acdaling <acdaling@gmail.com>
2020-11-29 21:35:16 +0100

    Observing profile data

--- app/src/main/java/com/example/capstone/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/capstone/ui/ProfileFragment.kt
@@ -1,11 +1,19 @@
 package com.example.capstone.ui
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
 import com.example.capstone.R
+import com.example.capstone.ui.viewmodels.ProfileViewModel
+import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -13,10 +21,7 @@ import com.example.capstone.R
  * create an instance of this fragment.
  */
 class ProfileFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val viewModel: ProfileViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +29,20 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeProfile()
+    }
+
+    private fun observeProfile() {
+        viewModel.getProfile("064012345")
+        viewModel.profile.observe(viewLifecycleOwner, {
+            Glide.with(this).load(it.profile_picture).into(imgUserProfileFragment)
+            txtViewPhoneNumberProfileFragment.setText(it.phone_number)
+            txtViewEmailProfileFragment.setText(it.email_address)
+        })
     }
 
 }
