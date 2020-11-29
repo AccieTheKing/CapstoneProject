9687dd08c4eeb9ab5bae89ec668bc9e6e7a2b93e
Acdaling <acdaling@gmail.com>
2020-11-29 17:52:03 +0100

    added the profile router to the entry file

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,10 @@ app.use(cors());
 
 const announcementsRouter = require('./src/routes/announcements');
 const productsRouter = require('./src/routes/products');
-// const productsRouter = require('./src/routes/products');
+const profileRouter = require('./src/routes/profile.router');
 
 app.use('/announcements', announcementsRouter);
 app.use('/products', productsRouter);
+app.use('/profile', profileRouter);
 
 app.listen(port, () => console.log(`App listens to port ${port}`));
