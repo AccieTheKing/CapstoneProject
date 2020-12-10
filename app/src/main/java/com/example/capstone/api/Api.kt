f9f0a61ed6f8474a199737db2788a94147e887f8
Acdaling <acdaling@gmail.com>
2020-12-10 21:02:15 +0100

    Simplified the use of created api's

--- app/src/main/java/com/example/capstone/api/Api.kt
+++ app/src/main/java/com/example/capstone/api/Api.kt
@@ -1,25 +1,33 @@
 package com.example.capstone.api
 
 import com.example.capstone.services.ProductApiService
+import com.example.capstone.services.ProfileApiService
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class ProductApi {
+class ApiObject {
     companion object {
         // The base url off the api.
         private const val baseUrl = "http://192.168.178.199:5000/"
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
 
-        /**
-         * @return [ProductApiService] The service class off the retrofit client.
-         */
-        fun createApi(): ProductApiService {
-            // Create an OkHttpClient to be able to make a log of the network traffic
-            val okHttpClient = OkHttpClient.Builder()
-                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        fun createProfileApi(): ProfileApiService {
+            // Create the Retrofit instance
+            val profileApi = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
+            // Return the Retrofit ProfileApiService
+            return profileApi.create(ProfileApiService::class.java)
+        }
+
+        fun createProductApi(): ProductApiService {
             // Create the Retrofit instance
             val productApi = Retrofit.Builder()
                 .baseUrl(baseUrl)
