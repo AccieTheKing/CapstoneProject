226e2ae0c1c6b76656900b84d0417a3ac3f1c71a
Acdaling <acdaling@gmail.com>
2020-12-10 23:34:58 +0100

    created endpoint on the back-end to fetch announcement

--- backend/src/routes/announcements.js
+++ backend/src/routes/announcements.js
@@ -6,4 +6,9 @@ router.get('/', async (req, res) => {
   res.json(announcement_dataset);
 });
 
+router.get('/:id', async (req, res) => {
+  const found_announcement = announcement_dataset[req.params.id];
+  if (found_announcement) res.json(found_announcement);
+});
+
 module.exports = router;
