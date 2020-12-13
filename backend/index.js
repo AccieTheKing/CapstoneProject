1ecd4fe206d64872e6e19c9a6fcb228b44e2dbf4
Acdaling <acdaling@gmail.com>
2020-12-14 00:29:11 +0100

    imported splash screen router

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,11 @@ app.use(cors());
 const announcementsRouter = require('./src/routes/announcements');
 const productsRouter = require('./src/routes/products');
 const profileRouter = require('./src/routes/profile.router');
+const splashScreenRouter = require('./src/routes/splash.router');
 
 app.use('/announcement', announcementsRouter);
 app.use('/product', productsRouter);
 app.use('/profile', profileRouter);
+app.use('/splashscreen', splashScreenRouter);
 
 app.listen(port, () => console.log(`App listens to port ${port}`));
