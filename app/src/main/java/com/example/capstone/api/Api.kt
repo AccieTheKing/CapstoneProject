2f08a739a29b6a28d032a7e94612abd7bd1cfef1
Acdaling <acdaling@gmail.com>
2020-12-24 15:17:14 +0100

    Refactored the creation of the API's, added interceptor for bearer token

--- app/src/main/java/com/example/capstone/api/Api.kt
+++ app/src/main/java/com/example/capstone/api/Api.kt
@@ -1,74 +1,65 @@
 package com.example.capstone.api
 
+import com.example.capstone.BuildConfig
+import com.example.capstone.repository.ProfileRepository
 import com.example.capstone.services.AnnouncementApiService
 import com.example.capstone.services.ProductApiService
 import com.example.capstone.services.ProfileApiService
+import com.example.capstone.services.SplashApiService
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
+import okio.IOException
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class Api {
     companion object {
-        // The base url off the api.
-        private const val baseUrl = "http://192.168.178.199:5000/"
+        private const val baseUrl: String = BuildConfig.DEVELOPMENT_SERVER_API
         private val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(object : Interceptor {
+                @Throws(IOException::class)
+                override fun intercept(chain: Interceptor.Chain): Response {
+                    val original: Request = chain.request()
+                    val request: Request = original.newBuilder()
+                        .header("Authorization", "Bearer ${ProfileRepository.authToken}")
+                        .method(original.method, original.body)
+                        .build()
+                    return chain.proceed(request)
+                }
+            })
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .build()
+        private val retroFitBuilder = Retrofit.Builder().baseUrl(baseUrl).client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
 
-        /**
-         * @return [AnnouncementApiService] The service class off the retrofit client.
-         */
         fun createAnnouncementApi(): AnnouncementApiService {
-            // Create an OkHttpClient to be able to make a log of the network traffic
-            val okHttpClient = OkHttpClient.Builder()
-                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-                .build()
-
             // Create the Retrofit instance
-            val announcementApi = Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .client(okHttpClient)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-
+            val announcementApi = retroFitBuilder.build()
             // Return the Retrofit AnnouncementApiService
             return announcementApi.create(AnnouncementApiService::class.java)
         }
 
         fun createProfileApi(): ProfileApiService {
             // Create the Retrofit instance
-            val profileApi = Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .client(okHttpClient)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-
+            val profileApi = retroFitBuilder.build()
             // Return the Retrofit ProfileApiService
             return profileApi.create(ProfileApiService::class.java)
         }
 
         fun createProductApi(): ProductApiService {
             // Create the Retrofit instance
-            val productApi = Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .client(okHttpClient)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-
+            val productApi = retroFitBuilder.build()
             // Return the Retrofit ProductApiService
             return productApi.create(ProductApiService::class.java)
         }
 
         fun createSplashApi(): SplashApiService {
             // Create the Retrofit instance
-            val splashApi = Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .client(okHttpClient)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-
-            // Return the Retrofit ProductApiService
+            val splashApi = retroFitBuilder.build()
+            // Return the Retrofit SplashApiService
             return splashApi.create(SplashApiService::class.java)
         }
     }
