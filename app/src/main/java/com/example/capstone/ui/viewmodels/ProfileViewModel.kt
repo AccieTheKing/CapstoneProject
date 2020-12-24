792f54050b067bc24bfeccb25b2a2901374a6e8b
Acdaling <acdaling@gmail.com>
2020-12-24 01:07:14 +0100

    Added methods for sending verification code in viewModel

--- app/src/main/java/com/example/capstone/ui/viewmodels/ProfileViewModel.kt
+++ app/src/main/java/com/example/capstone/ui/viewmodels/ProfileViewModel.kt
@@ -14,6 +14,7 @@ class ProfileViewModel(application: Application) : AndroidViewModel(application)
     private val _errorText: MutableLiveData<String> = MutableLiveData()
 
     val profile = profileRepository.profile
+    val tokenSuccess = profileRepository.success
     val success = MutableLiveData<Boolean>()
     val errorText: LiveData<String> get() = _errorText
 
@@ -39,6 +40,32 @@ class ProfileViewModel(application: Application) : AndroidViewModel(application)
                 success.value = false
                 _errorText.value = error.message
                 Log.e("Updating profile error", error.cause.toString())
+            }
+        }
+    }
+
+    fun getVerificationCode(phone_number: String, email_address: String) {
+        viewModelScope.launch {
+            try {
+                profileRepository.getVerificationCode(phone_number, email_address)
+                success.value = true
+            } catch (error: ProfileRepository.ProfileApiError) {
+                success.value = false
+                _errorText.value = error.message
+                Log.e("Wrong verification code", error.cause.toString())
+            }
+        }
+    }
+
+    fun sendVerificationCode(code: String, phone_number: String, email_address: String) {
+        viewModelScope.launch {
+            try {
+                profileRepository.sendVerificationCode(code.toInt(), phone_number, email_address)
+                success.value = true
+            } catch (error: ProfileRepository.ProfileApiError) {
+                success.value = false
+                _errorText.value = error.message
+                Log.e("Wrong verification code", error.cause.toString())
             }
         }
     }
