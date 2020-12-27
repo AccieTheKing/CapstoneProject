076d616329f2525aab109852fb8271b5b58546fc
Acdaling <acdaling@gmail.com>
2020-12-28 00:08:49 +0100

    Created function that are based on the profile model

--- backend/src/database/models/profile.model.js
+++ backend/src/database/models/profile.model.js
@@ -9,4 +9,33 @@ const ProfileSchema = new Schema({
 
 const Profile = mongoose.model('Profile', ProfileSchema);
 
-module.exports = Profile;
+const findByEmail = async (email) => {
+  const foundProfile = await Profile.findOne({ email });
+  if (foundProfile) return foundProfile;
+  return false;
+};
+
+const findByPhoneNumber = async (phoneNumber) => {
+  const foundProfile = await Profile.findOne({ phoneNumber });
+  if (foundProfile) return foundProfile;
+  return false;
+};
+
+const storeProfile = async (email, phoneNumber, verificationCode) => {
+  try {
+    const profileModel = new ProfileModel({
+      email,
+      phoneNumber,
+      verificationCode,
+    });
+    profileModel.save();
+  } catch (error) {
+    console.log(`something went wrong with saving the profileModel: ${error}`);
+  }
+};
+
+module.exports = {
+  storeProfile,
+  findByEmail,
+  findByPhoneNumber,
+};
