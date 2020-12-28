8e30bcdc2bcfd68ac7e47fb45c9d650b58ea683d
Acdaling <acdaling@gmail.com>
2020-12-28 01:02:27 +0100

    secured the announcement routes

--- backend/src/routes/models/announcement.router.js
+++ backend/src/routes/models/announcement.router.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
+const { checkToken } = require('../../libs');
 const announcement_dataset = require('../data/announcement.json');
 
-router.get('/', async (req, res) => {
+router.get('/', checkToken, async (req, res) => {
   res.json(announcement_dataset);
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', checkToken, async (req, res) => {
   const found_announcement = announcement_dataset[req.params.id];
   if (found_announcement) res.json(found_announcement);
 });
